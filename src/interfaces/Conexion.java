ac18107564c18c95a8ef121ebc510d3d7772450c
AngelC <acaiza0272@uta.edu.ec>
2022-06-17 10:53:45 -0500

    Trabajo en la conexion , realizo la conexion a la base de datos

--- src/interfaces/Conexion.java
+++ src/interfaces/Conexion.java
@@ -5,10 +5,36 @@
  */
 package interfaces;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author USUARIO
  */
 public class Conexion {
+     Connection con;
+    public ResultSet resultado;
+    public Statement sentencia;
+
+    public Connection conectar() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/iniciarsesion", "root", "");
+            sentencia = con.createStatement();
+
+            //System.out.println("Conexion Exitosa");
+        } catch (Exception ex) {
+            //System.out.println(e);
+            JOptionPane.showMessageDialog(null, ex);
+        }
+        return con;
+
+    }
     
 }
